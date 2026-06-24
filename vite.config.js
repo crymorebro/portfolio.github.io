0fdd3eebf0eddf86767a8eb7d8b662437fc66113
crymorebro <kolakudasev1840@gmail.com>
2026-06-24 14:06:00 +0300

    fix: change base path to root slash for user domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/portfolio.github.io/', 
+  base: '/', 
   plugins: [
     vue(),
     vueDevTools(),
